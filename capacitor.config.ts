4a6a9879ff7d2ff32b3e75859753d53b429c3d81
pslyman <dev@porterlyman.com>
2025-07-06 13:28:33 -0600

    cleanup and fix all packages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.WaterfallWorkouts',
+  appId: 'com.waterfallworkouts',
   appName: 'WaterfallWorkouts',
-  webDir: 'www',
+  webDir: 'www/browser',
   cordova: {
     preferences: {
       ScrollEnabled: 'false',
